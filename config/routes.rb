896b5f81c756bbdea4e41797bb4b9c3ebf0035ac
BON-BON2 <bonyoshi201903@gmail.com>
2023-10-10 22:53:41 +0900

    12-6 beforeアクション

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   # get '/' => 'articles#index'
   # get '/about' => 'articles#about'
-  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
+  # resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :articles
 end
